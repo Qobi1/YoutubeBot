f4e1a15ed4f54bef0111e517056c7af2e0ee1029
Qobi1 <tolaganov03@list.ru>
2022-06-09 14:25:45 +0500

    'done'

--- VideoDownloader/botapp/management/commands/runbot.py
+++ VideoDownloader/botapp/management/commands/runbot.py
@@ -2,14 +2,15 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from django.core.management import BaseCommand
 
 
-from VideoDownloader.botapp.views import start
+from botapp.views import *
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         updater = Updater('5028779716:AAEWI_822MoMa8GKg2wADRNKkTBvI0eujA4')
         updater.dispatcher.add_handler(CommandHandler('start', start))
-        # updater.dispatcher.add_handler(MessageHandler(Filters.text, start))
+        # updater.dispatcher.add_handler(MessageHandler(Filters.entity('url'), received_link))
+        updater.dispatcher.add_handler(MessageHandler(Filters.text, received_message))
 
         updater.start_polling()
         updater.idle()
--- VideoDownloader/botapp/views.py
+++ VideoDownloader/botapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from telegram.ext import CallbackContext
 from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
 from .models import *
+from pytube import YouTube
 
 # Create your views here.
 
@@ -12,30 +13,56 @@ def start(update: Update, context: CallbackContext):
     if log is None:
         log = User()
         log.user_id = user.id
-        log.log = {'state': 0}
-        log.save()
-    update.message.reply_text(f"""🇦🇺 - Choose the language\n🇷🇺 - Выбирай язык\n🇺🇿 - Tilni tanlang""", reply_markup=btns(type='language'))
+    update.message.reply_text(f"""🇦🇺 - Good Day! Send me Youtube link to download video\n
+🇷🇺 - Здраствуйте, пришлите мне ссылку на Youtube для скачивания видео\n
+🇺🇿 - Assalomu alaykum, Youtube silkani tashlang videoni skachat qlish uchun""")
+
+    log.log = {'state': 0}
+    log.save()
+
+
+def received_message(update: Update, context: CallbackContext):
+    msg = update.message.text
+    user = update.effective_user
+    log = User.objects.filter(user_id=user.id).first()
+    state = log.log
+    print('>>>>>>>>>>>>>>>>>', msg)
+    if state['state'] == 0:
+        yt = YouTube(msg)
+        filename = yt.title
+        print(filename)
+        s = yt.streams.get_by_itag(17)
+        s.download(output_path='video')
+        update.message.reply_text("""🇦🇺 - Please wait..\n🇷🇺 - Пожалуйста, подождите..\n🇺🇿 - Iltimos kuting..""")
+        context.bot.send_video(video=open(f'video/{filename}.3gpp', 'rb'), chat_id=user.id, timeout=1000)
+    log.log = state
+    log.save()
+
+
+# def received_link(update: Update, context: CallbackContext):
+#     user = update.effective_user
+#     msg = update.message.text
+#     log = User.objects.filter(user_id=user.id).first()
+#     state = log.log
+#     if state['state'] == 0:
+#         yt = YouTube(msg)
+#         filename = yt.title
+#         print(filename)
+#         s = yt.streams.get_by_itag(17)
+#         s.download(output_path='video')
+#         update.message.reply_text("""🇦🇺 - Please wait..\n🇷🇺 - Пожалуйста, подождите..\n🇺🇿 - Iltimos kuting..""")
+#         context.bot.send_video(video=open(f'video/{filename}.3gpp', 'rb'), chat_id=user.id, timeout=1000)
 
 
 def btns(type=None):
-    if type == 'language':
+    btn = []
+    if type == 'format':
         btn = [
-            [KeyboardButton('🇦🇺Eng'), KeyboardButton('🇷🇺Rus')],
-            [KeyboardButton('🇺🇿Uzb')]
-               ]
+            [KeyboardButton('180'), KeyboardButton('360')],
+            [KeyboardButton('720')],
+            [KeyboardButton('Audio')]
+        ]
+
     else:
         pass
-
-
-def language():
-    dictionary = {
-        'uzb': {
-
-        },
-        'rus': {
-
-        },
-        'en': {
-
-        }
-    }
+    return ReplyKeyboardMarkup(btn, resize_keyboard=True, one_time_keyboard=True)
