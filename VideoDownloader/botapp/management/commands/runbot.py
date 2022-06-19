2724d7397a31c7e4722ccff6f7ea2dad1c7dba4c
Qobi1 <tolaganov03@list.ru>
2022-06-19 11:03:53 +0500

    'done'

--- VideoDownloader/botapp/management/commands/runbot.py
+++ VideoDownloader/botapp/management/commands/runbot.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         updater = Updater('5028779716:AAEWI_822MoMa8GKg2wADRNKkTBvI0eujA4')
         updater.dispatcher.add_handler(CommandHandler('start', start))
-        # updater.dispatcher.add_handler(MessageHandler(Filters.entity('url'), received_link))
+        updater.dispatcher.add_handler(MessageHandler(Filters.entity('url'), received_link))
         updater.dispatcher.add_handler(MessageHandler(Filters.text, received_message))
 
         updater.start_polling()
--- VideoDownloader/botapp/views.py
+++ VideoDownloader/botapp/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from telegram.ext import CallbackContext
 from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
 from .models import *
-from pytube import YouTube
+from pytube import YouTube, Stream
+import os
 
 # Create your views here.
 
@@ -26,32 +27,47 @@ def received_message(update: Update, context: CallbackContext):
     user = update.effective_user
     log = User.objects.filter(user_id=user.id).first()
     state = log.log
-    print('>>>>>>>>>>>>>>>>>', msg)
-    if state['state'] == 0:
-        yt = YouTube(msg)
-        filename = yt.title
-        print(filename)
-        s = yt.streams.get_by_itag(17)
-        s.download(output_path='video')
+    if state['state'] == 1:
+        msg = msg.split(' ')
+        print(msg)
+        itag = 0
+        type = ''
+        if msg[0] == '180':
+            print(">>>", msg[0])
+            itag = 17
+            type = '3gpp'
+        elif msg[0] == '360':
+            itag = 18
+            type = 'mp4'
+        elif msg[0] == '720':
+            itag = 22
+            type = 'mp4'
+        elif msg[0] == 'Audio':
+            itag = 251
+            type = 'webm'
         update.message.reply_text("""🇦🇺 - Please wait..\n🇷🇺 - Пожалуйста, подождите..\n🇺🇿 - Iltimos kuting..""")
-        context.bot.send_video(video=open(f'video/{filename}.3gpp', 'rb'), chat_id=user.id, timeout=1000)
+        yt = YouTube(state['link'])
+        filename = yt.title
+        yt = yt.streams.get_by_itag(itag)
+        yt.download(output_path='video')
+        context.bot.send_video(video=open(f'video/{filename}.{type}', 'rb'), chat_id=user.id, timeout=1000)
+        # os.remove(f'video/{filename}.{type}')
     log.log = state
     log.save()
 
 
-# def received_link(update: Update, context: CallbackContext):
-#     user = update.effective_user
-#     msg = update.message.text
-#     log = User.objects.filter(user_id=user.id).first()
-#     state = log.log
-#     if state['state'] == 0:
-#         yt = YouTube(msg)
-#         filename = yt.title
-#         print(filename)
-#         s = yt.streams.get_by_itag(17)
-#         s.download(output_path='video')
-#         update.message.reply_text("""🇦🇺 - Please wait..\n🇷🇺 - Пожалуйста, подождите..\n🇺🇿 - Iltimos kuting..""")
-#         context.bot.send_video(video=open(f'video/{filename}.3gpp', 'rb'), chat_id=user.id, timeout=1000)
+def received_link(update: Update, context: CallbackContext):
+    user = update.effective_user
+    link = update.message.text
+    log = User.objects.filter(user_id=user.id).first()
+    state = log.log
+    if 0 <= state['state'] <= 1:
+        state['link'] = link
+        state['state'] = 1
+        update.message.reply_text("🇦🇺 - Choose the format\n🇷🇺 - Выберите формат\n🇺🇿 - Formatni tanlang",
+                                  reply_markup=btns(type='format'))
+    log.log = state
+    log.save()
 
 
 def btns(type=None):
@@ -63,6 +79,4 @@ def btns(type=None):
             [KeyboardButton('Audio')]
         ]
 
-    else:
-        pass
     return ReplyKeyboardMarkup(btn, resize_keyboard=True, one_time_keyboard=True)
